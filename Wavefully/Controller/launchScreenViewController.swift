b52c19f4bbefaeff22a696f2b4870d88f7e0e7fd
Christopher Davis <chris@christopheralandavis.com>
2018-12-08 14:48:06 -0500

    Added a launch screen and fixed a bug with the pods.

---
+++ Wavefully/Controller/launchScreenViewController.swift
@@ -0,0 +1,10 @@
+//
+//  launchScreenViewController.swift
+//  Wavefully
+//
+//  Created by Christopher Davis on 12/8/18.
+//  Copyright © 2018 Social Pilot. All rights reserved.
+//
+
+import Foundation
+import UIKit
