56e081e4e8f42763b33cf7b4c7ff40db28fe4d7c
Christopher Davis <chris@christopheralandavis.com>
2019-01-13 16:58:43 -0500

    Allow the app to open Twitter from Settings Faded out the App Store Rating for now Set up a placeholder View Controller for the Thanks page

--- Wavefully/Controller/SettingsViewController.swift
+++ Wavefully/Controller/SettingsViewController.swift
@@ -16,8 +16,18 @@ class SettingsViewController: UITableViewController {
     // MARK: - Actions
     
     
+    
+    
     // MARK: - Outlets
-
+    @IBOutlet weak var AllowNotificationsCell: UITableViewCell!
+    @IBOutlet weak var CustomNotificationTimeCell: UITableViewCell!
+    @IBOutlet weak var CustomNotficationSetCell: UITableViewCell!
+    @IBOutlet weak var LeaveFeedbackCell: UITableViewCell!
+    @IBOutlet weak var TweetAppCell: UITableViewCell!
+    @IBOutlet weak var RateAppCell: UITableViewCell!
+    @IBOutlet weak var FollowCreatorCell: UITableViewCell!
+    @IBOutlet weak var ThanksCell: UITableViewCell!
+    
     
     
     
@@ -39,6 +49,72 @@ class SettingsViewController: UITableViewController {
     
     // MARK: - FUNCTIONS
     
+    
+    // Functions for acting on whatever row you tap.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            print("You want to allow notifications.")
+        }
+        else if indexPath.section == 0 && indexPath.row == 1 {
+            print("You want to set a custom time.")
+        }
+
+
+        if indexPath.section == 1 && indexPath.row == 0 {
+            BugReporting.invoke()
+        }
+        else if indexPath.section == 1 && indexPath.row == 1 {
+            launchAppTwitter()
+        }
+
+
+        if indexPath.section == 2 && indexPath.row == 0 {
+            print("You want to rate the app.")
+        }
+
+
+        if indexPath.section == 3 && indexPath.row == 0 {
+            launchMyTwitter()
+        }
+        else if indexPath.section == 3 && indexPath.row == 1 {
+            print("Opened")
+        }
+
+    }
+    
+    
+    
+    // Function for opening Twitter if possible
+    // Open the Twitter app to my profile, or open to the web.
+    func launchMyTwitter() {
+        let screenName = "ObviousUnrest"
+        let appURL = NSURL(string: "twitter://user?screen_name=\(screenName)")!
+        let webURL = NSURL(string: "https://twitter.com/\(screenName)")!
+        
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            application.open(webURL as URL)
+        }
+    }
+    
+    // Open the Twitter app to my app's profile, or open to the web.
+    func launchAppTwitter() {
+        let screenName = "ObviousUnrest"
+        let appURL = NSURL(string: "twitter://user?screen_name=\(screenName)")!
+        let webURL = NSURL(string: "https://twitter.com/\(screenName)")!
+        
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            application.open(webURL as URL)
+        }
+    }
     
     
     
---
+++ Wavefully/Controller/ThanksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThanksViewController.swift
+//  Wavefully
+//
+//  Created by Christopher Davis on 1/13/19.
+//  Copyright © 2019 Social Pilot. All rights reserved.
+//
+
+import UIKit
+
+class ThanksViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
