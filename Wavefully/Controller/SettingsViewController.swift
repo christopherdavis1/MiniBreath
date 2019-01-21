7be9a98011258b808f3333264d26c5c8836b5340
Christopher Davis <chris@christopheralandavis.com>
2019-01-21 16:36:17 -0500

    Push notifications work!

--- Wavefully/Controller/SettingsViewController.swift
+++ Wavefully/Controller/SettingsViewController.swift
@@ -36,6 +36,7 @@ class SettingsViewController: UITableViewController {
     @IBOutlet weak var ThanksCell: UITableViewCell!
     @IBOutlet weak var AllowNotificationsSwitch: UISwitch!
     @IBOutlet weak var SetCustomTimeSwitch: UISwitch!
+    @IBOutlet weak var notificationsTimingLabel: UILabel!
     
     
     
@@ -138,77 +139,89 @@ class SettingsViewController: UITableViewController {
     
     
     // MARK: - Functions for handling notifications switches
+    
     // Allow Notifications Switch
     func AllowNotificationsSwitchChanged() {
-        
-        checkNotificationSettings()
-        
-        if AllowNotificationsSwitch.isOn {
-            print("Notifications are allowed.")
-        } else {
-            print("Notifications are not allowed.")
+        if notificationsOn == false {
+            // if the notifications haven't been allowed yet:
+            registerNotifications()
+            scheduleDefaultPushNotifications()
+            AllowNotificationsSwitch.setOn(true, animated: true)
+            notificationsAllowed = true
+            print("Notifications are on and allowed")
             
+            // If notifications are allowed and turned on already:
+        } else if notificationsAllowed == true && notificationsOn == true {
+            // If notifications are both allowed and ON when the switch is tapped...
+            // Remove pending notification requests
+            center.removeAllPendingNotificationRequests()
+            // And turn the switch off...
+            AllowNotificationsSwitch.setOn(false, animated: true)
+            // And set the allowed to false:
+            notificationsAllowed = false
+            notificationsTimingLabel.text = "Notifications are turned off."
+            
+            // If notifications are allowed, but turned off:
+        } else if notificationsOn == true && notificationsAllowed == false {
+            // Reset the schedule
+            scheduleDefaultPushNotifications()
+            // Turn the switch back on:
+            AllowNotificationsSwitch.setOn(true, animated: true)
+            // And tell the app that notifications are allowed...
+            notificationsAllowed = true
         }
     }
+    
+    
     
     // Allow Custom Time Switch
-    func setCustomTimeSwitchChanged() {
-        if SetCustomTimeSwitch.isOn {
-            print("You set a new custom time.")
-        } else {
-            print("You are using the standard time.")
-        }
-    }
+    func setCustomTimeSwitchChanged() {}
     
     
     
-    // MARK: - Ask for and managing notifications
     
-    // Ask for the ability to send notifications
-    // If the user has turned it off, we'll know.
+    // MARK: - Turning on / off push notifications & setting timing
     
-    // TO-DO: - Notifications needs a lot of work!
-    
-    func registerForPushNotifications() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
-            [weak self] granted, error in
-
-            print("Permission granted: \(granted)")
-            guard granted else {return}
-            self?.getNotificationSettings()
-        }
-    }
-
-    // Check to see whether or not notifications are allowed
-    func getNotificationSettings() {
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            print("Notification Settings: \(settings)")
-
-            guard settings.authorizationStatus == .authorized else { return }
-            DispatchQueue.main.async {
-                UIApplication.shared.registerForRemoteNotifications()
-            }
-        }
-    }
-
-    
-    func checkNotificationSettings() {
-        
-        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
-         
-            if settings.authorizationStatus == .notDetermined {
-                print("Notification permission hasn't been asked yet!")
-                self.registerForPushNotifications()
-            } else if settings.authorizationStatus == .authorized {
-                print("The user has authorized notifications.")
-            } else if settings.authorizationStatus == .denied {
-                print("The user has denied notifications.")
-            }
+    // Function for registering notifictions:
+    @objc func registerNotifications() {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
             
+            if granted {
+                self.notificationsOn = true
+                print("\("Notifications are") \(self.notificationsOn)")
+                print("Granted")
+            } else {
+                print("\("Notifications are") \(self.notificationsOn)")
+                print("Not Granted")
+            }
         }
     }
     
-
+    
+    // Function for setting a custom push notification time:
+    @objc func scheduleDefaultPushNotifications() {
+        
+        // What does the content say?
+        let content = UNMutableNotificationContent()
+        content.title = "Mindful Moment Reminder"
+        content.body = "Take a 10 second pause, and reflect on something inspiring."
+        content.categoryIdentifier = "reminder"
+        content.sound = UNNotificationSound.default
+        
+        // The timing:
+        
+        // Un-Comment to show the notification every day at 2:00pm EST
+        //        let dateComponents = DateComponents()
+        //        dateComponents.hour = 14
+        //        dateComponents.minute = 00
+        //        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        notificationsTimingLabel.text = "\("Notifications are set for") \(trigger.timeInterval) \("seconds from now.")"
+        
+        // Show the notification content at the time assigned!
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
     
     
     
